678a16b92caebb41f86f0eba8306e56510b18d6e
hugooisraell <hugoolai05@gmail.com>
2025-02-05 01:14:19 -0500

    Nuevo script sql, correcto

--- sistemaeducativo.sql
+++ sistemaeducativo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-01-2025 a las 22:48:32
+-- Tiempo de generación: 05-02-2025 a las 07:13:00
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sistemaeducativo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactos`
+--
+
+CREATE TABLE `contactos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `contactos`
+--
+
+INSERT INTO `contactos` (`id`, `nombre`, `email`, `telefono`, `apellido`) VALUES
+(1, 'Jorge', 'jorge2025@ejemplo.com', '0945683425', 'Lopez'),
+(2, 'Maria', 'maria72@ejemplo.com', '0991242566', 'Catañeda'),
+(3, 'Ximena', 'xime_na22@ejemplo.com', '0932265242', 'Ximénez');
 
 -- --------------------------------------------------------
 
@@ -42,16 +65,41 @@ CREATE TABLE `eventos` (
 --
 
 INSERT INTO `eventos` (`id`, `titulo`, `hora`, `fecha`, `categoria`, `descripcion`, `lugar`) VALUES
-(3, 'Conferencia sobre Tecnología', '09:00:00', '2025-01-10', 'Conferencia', 'Una conferencia sobre avances tecnológicos', 'Auditorio A'),
-(4, 'Conferencia de PHP', '10:30:00', '2024-12-30', 'Tecnología', 'Una conferencia sobre desarrollo web con PHP', 'Auditorio Central'),
-(6, 'CSS en Diseño web', '10:00:00', '2024-12-28', 'Tecnología', 'Una conferencia sobre diseño web con CSS', 'Aula Virtual'),
+(4, 'Conferencia de PHP', '10:30:00', '2024-12-30', 'Desarrollo Web', 'Una conferencia sobre desarrollo web con PHP', 'Auditorio Central'),
 (7, 'Cyber Security', '19:00:00', '2025-01-10', 'Tecnologia', 'Conferencia de seguridad informática', 'Auditorio C2'),
-(8, 'Taller de Moodle', '15:00:00', '2025-01-30', 'Tecnologia', 'Charla sobre el desarrollo de cursos en Moodle', 'Aula 35'),
-(9, 'Reunión Sistemas', '16:00:00', '2025-02-06', 'Reunión', 'Reunión sobre desarrollo de una Base de Datos', 'Auditorio Central');
+(9, 'Reunión Sistemas', '16:00:00', '2025-02-06', 'Reunión', 'Reunión sobre desarrollo de una Base de Datos', 'Auditorio Central'),
+(10, 'Reunión de HCI', '19:00:00', '2025-01-15', 'Desarrollo GUI', 'Reunión sobre desarrollo GUI', 'Aula Virtual'),
+(11, 'Diseño de Interfaz', '20:00:00', '2025-02-06', 'Frontend 1', 'Charla sobre CSS y Boostrap', 'Aula 38'),
+(12, 'Cyber Security', '17:30:00', '2025-02-28', 'Tecnologia', 'Conferencia de seguridad informática', 'Auditorio C2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicaciones`
+--
+
+CREATE TABLE `ubicaciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `direccion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ubicaciones`
+--
+
+INSERT INTO `ubicaciones` (`id`, `nombre`, `direccion`) VALUES
+(3, 'Plaza Central - Cede Matriz', 'Universidad UTE');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `eventos`
@@ -60,14 +108,32 @@ ALTER TABLE `eventos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
